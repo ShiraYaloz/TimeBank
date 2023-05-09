57788740735ac0ae0db1db204804688ad6f225aa
ShiraYaloz <s3231871@gmail.com>
2023-05-09 20:13:55 +0300

    all

---
+++ server/TimeBank/Bll/functions/memberFunctions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bll.functions
+{
+    public class memberFunctions
+    {
+        public static Dto.dtoClasses.member getMemberByPhone(string name)
+        {
+            return Bll.memberConvert.convertFromMicToDto
+                (Dal.functions.memberFun.getMemberByPhone(name));
+        }
+        public static List<Dto.dtoClasses.member> GetAllMembersBll()
+        {
+            return Bll.memberConvert.ConvertListFromMicToDto(Dal.functions.memberFun.GetAllMembers());
+        }
+        public static void addMember(Dto.dtoClasses.member mnew)
+        {
+            Dal.functions.memberFun.addMember(memberConvert.convertFromDtoToMicro(mnew));
+        }
+        public static void approveMember(string phone)
+        {
+            Dal.functions.memberFun.approveMember(phone);
+        }
+        public static Dto.dtoClasses.member getMemberByPhoneAndPass(string phone, string pass)
+        {
+            return Bll.memberConvert.convertFromMicToDto( Dal.functions.memberFun.getMemberByPhoneAndPass(phone,pass)) ;
+        }
+    }
+}

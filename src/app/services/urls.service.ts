1caf14b81e93ee456ab0e7b59c46da87257d4000
Dini-n <dinatova1@gmail.com>
2023-05-10 10:25:19 +0300

    הי שירה 3

--- src/app/services/urls.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable({
-  providedIn: 'root'
-})
-export class UrlsService {
-  urlApi = "https://localhost:44357/api/";
-  constructor() { }
-}

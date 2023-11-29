fedbb08dc97692d6b9a99def2a4a2ddb2e46090c
Dini-n <dinatova1@gmail.com>
2023-11-29 20:50:50 +0200

    d

---
+++ angular/TimeBank/src/app/services/search-category-memeber.service.ts
@@ -0,0 +1,47 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { MemPlusCat } from '../classes/mem-plus-cat';
+import { UrlsService } from './urls.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SearchCategoryMemeberService {
+  categoryController = "memberCategory/"
+  constructor(private http:HttpClient , private urls:UrlsService) { }
+  urlApi = this.urls.urlApi;
+ 
+ 
+ /* getFilterMemberCategory(memPlusCat:MemPlusCat):Observable<Array<MemPlusCat>>
+  {
+    return this.http.post<Array<MemPlusCat>>(this.urlApi + this.categoryController+"getFilterMemberCategory",memPlusCat);
+  }
+  */
+  getFilterMemberCategory(memPlusCat: MemPlusCat): Observable<Array<MemPlusCat>> {
+    return this.http.post<Array<MemPlusCat>>(this.urlApi + this.categoryController + "getFilterMemberCategory", memPlusCat);
+  }
+  
+
+  memberCategorySearchResult :Array<MemPlusCat> = new Array<MemPlusCat>();
+ 
+  loadData(memPlusCat:MemPlusCat):boolean
+  {   
+    this.getFilterMemberCategory(memPlusCat).subscribe(
+    
+      (myData)=>
+     { 
+       this.memberCategorySearchResult = myData;      
+       return true;  
+     },
+       (myErr)=>
+     { 
+        alert(myErr.message);
+        return false
+      }
+    );
+    return true 
+  }
+}
+
+
